387b278471148d5ecf7f6d352440a8352103bc04
Heppy Andrew <heppyandrew@Heppys-MacBook-Pro.local>
2021-07-18 14:30:03 +0800

    main

--- js/investors.js
+++ js/investors.js
@@ -11,3 +11,34 @@ function Home(event) {
   // ORIGINAL LINK
   location.replace("./index.html");
 }
+
+
+
+
+// -------------------Mobile Version---------------------------------
+
+// Navbar appearing
+$(document).ready(function () {
+  $(".NavBar-mobile-lines").click(function () {
+    // $("#navbar").toggleClass("NavBar-mobile-show");
+
+    // hide / show navbar
+    if ($("#navbar").hasClass("NavBar-mobile-hide")) {
+      $("#navbar").removeClass("NavBar-mobile-hide");
+      $("#navbar").removeClass("NavBar-mobile-list");
+      $("#navbar").addClass("NavBar-mobile-show");
+    } else {
+      $("#navbar").removeClass("NavBar-mobile-show");
+      $("#navbar").addClass("NavBar-mobile-hide");
+    }
+
+    // switch icon
+    if ($("#NavBar-icon").hasClass("fa-bars")) {
+      $("#NavBar-icon").removeClass("fa-bars");
+      $("#NavBar-icon").addClass("fa-sign-out-alt");
+    } else {
+      $("#NavBar-icon").removeClass("fa-sign-out-alt");
+      $("#NavBar-icon").addClass("fa-bars");
+    }
+  });
+});
